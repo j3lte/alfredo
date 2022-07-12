f8bceeb234711be28f965f7ef11b91b08856a9a4
j3lte <jwlagendijk@gmail.com>
2022-07-12 18:44:51 +0200

    Update cache && deps

--- deps.ts
+++ deps.ts
@@ -1,14 +1,5 @@
-export {
-  dirname,
-  join,
-  parse,
-  resolve,
-} from "https://deno.land/std@0.147.0/path/mod.ts";
-export {
-  ensureDir,
-  ensureSymlink,
-  walk,
-} from "https://deno.land/std@0.147.0/fs/mod.ts";
+export { dirname, join, parse, resolve } from "https://deno.land/std@0.147.0/path/mod.ts";
+export { ensureDir, ensureSymlink, walk } from "https://deno.land/std@0.147.0/fs/mod.ts";
 
 export { parseBuffer } from "https://deno.land/x/bplist_parser@0.2.1/mod.ts";
 export { cryptoRandomString as crStr } from "https://deno.land/x/crypto_random_string@1.1.0/mod.ts";
