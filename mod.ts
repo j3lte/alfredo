ac55cf66ef152851514f70cbc786b77c2da46e05
j3lte <jwlagendijk@gmail.com>
2022-07-12 16:29:53 +0200

    Update version

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
 export * from "./lib/alfredo/typed.d.ts";
+export { withPermissions } from "./lib/utils/permissions.ts";
 export { Alfredo, alfredo } from "./lib/alfredo/mod.ts";
-export const VERSION = "0.2.3";
+export const VERSION = "0.3.0";
